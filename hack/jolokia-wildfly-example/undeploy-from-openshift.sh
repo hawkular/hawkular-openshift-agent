d2984d852a538794b34db3957c001cb680e48325
John Mazzitelli <mazz@redhat.com>
2016-11-21 12:40:15 -0500

    minor: fix typos

--- hack/jolokia-wildfly-example/undeploy-from-openshift.sh
+++ hack/jolokia-wildfly-example/undeploy-from-openshift.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# The OpenShift project where the example Jolokia endpoint is to be deployed
+# The OpenShift project where the example Jolokia WildFly endpoint is deployed
 OS_PROJECT=${1:-openshift-infra}
 
-echo Will undeploy example Jolokia Wildfly endpoint from project $OS_PROJECT
+echo Will undeploy example Jolokia WildFly endpoint from project $OS_PROJECT
 
 # Login and switch to the target project
 oc login
 oc project $OS_PROJECT
 
-# Deploy the example Prometheus endpoint
+# Undeploy the example Jolokia WildFly endpoint
 oc process -f jolokia-wildfly.yaml | oc delete -f -
 oc delete -f jolokia-wildfly-configmap.yaml
