5859a851259f04f07a746dcf3a9c1e53cf25c770
John Mazzitelli <mazz@redhat.com>
2016-11-21 12:51:07 -0500

    minor: fix typos

--- hack/jolokia-wildfly-example/deploy-to-openshift.sh
+++ hack/jolokia-wildfly-example/deploy-to-openshift.sh
@@ -3,12 +3,12 @@
 # The OpenShift project where the example Jolokia endpoint is to be deployed
 OS_PROJECT=${1:-openshift-infra}
 
-echo Will deploy example Jolokia Wildfly endpoint to project $OS_PROJECT
+echo Will deploy example Jolokia WildFly endpoint to project $OS_PROJECT
 
 # Login and switch to the target project
 oc login
 oc project $OS_PROJECT
 
-# Deploy the example Prometheus endpoint
+# Deploy the example Jolokia WildFly endpoint
 oc process -f jolokia-wildfly.yaml | oc create -f -
 oc create -f jolokia-wildfly-configmap.yaml
